9b241e13205a7040c36f3d9f263f3df3357e1d0b
Rob <robburson07@gmail.com>
2022-02-12 20:41:17 -0600

    wrote my truly unimpressive resume section

---
+++ src/lib/resume.js
@@ -0,0 +1,13 @@
+const exp = [
+    {
+        id: 1,
+        title: 'Full Stack Developer',
+        company: 'Myself - Currently Freelance',
+        dates: 'September 2021 - Present',
+        description: [
+            'n/a'
+        ]
+    }
+];
+
+export default exp;
